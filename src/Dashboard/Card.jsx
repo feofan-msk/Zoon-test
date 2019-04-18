8e7d2b8afdd905bc49e75599392e6c3834362713
Denis Feofanov <omail4me@gmail.com>
2019-04-18 16:27:20 +0300

    style: add Dashboard style

--- src/Dashboard/Card.jsx
+++ src/Dashboard/Card.jsx
@@ -1,14 +1,52 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const S = {};
+S.Card = styled.div`
+  flex: 1 1 25%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 16px 14px 16px 26px;
+  border-radius: 4px;
+  background-color: #4a0594;
+  color: #ffffff;
+  overflow-wrap: anywhere;
+  cursor: pointer;
+
+  & + & {
+    margin-left: 5px;
+  }
+`;
+
+S.Title = styled.h4`
+  margin: 0 0 4px;
+  font-size: 25px;
+  font-weight: 700;
+`;
+
+S.Description = styled.p`
+  margin: 0;
+  font-size: 13px;
+  font-weight: 400;
+`;
+
+S.Icon = styled.img`
+  align-self: flex-start;
+`;
 
 class Card extends Component {
   render() {
     return (
-      <div>
-        <h4>{this.props.number}</h4>
-        <p>{this.props.description}</p>
+      <S.Card>
+        <div>
+          <S.Title>{this.props.number}</S.Title>
+          <S.Description>{this.props.description}</S.Description>
+        </div>
 
-        <img src={this.props.icon} alt="card icon" />
-      </div>
+        <S.Icon src={this.props.icon} alt="card icon" />
+      </S.Card>
     );
   }
 }
--- src/Dashboard/index.jsx
+++ src/Dashboard/index.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
-import Card from "./Card";
+import styled from "styled-components";
 
+import Card from "./Card";
 import commentIcon from "../assets/comment-cloud.svg";
 import unansweredCommentIcon from "../assets/unanswered-comment.svg";
 import updateIcon from "../assets/update.svg";
 import starIcon from "../assets/star.svg";
 
+const DashboardList = styled.section`
+  display: flex;
+`;
+
 class Dashboard extends Component {
   render() {
     return (
-      <section>
-        <Card number={165} description="отзывов" icon={commentIcon} />
+      <DashboardList>
+        <Card
+          number={165}
+          description="отзывов"
+          icon={commentIcon}
+        />
         <Card
           number={21}
           description="неотвеченный отзыв"
@@ -18,7 +27,7 @@ class Dashboard extends Component {
         />
         <Card number={50} description="обновлений" icon={updateIcon} />
         <Card number={4.5} description="средний рейтинг" icon={starIcon} />
-      </section>
+      </DashboardList>
     );
   }
 }

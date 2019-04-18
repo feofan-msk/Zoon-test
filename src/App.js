dd1c19177c3bc0474c5813cf9bab28680e948abf
Denis Feofanov <omail4me@gmail.com>
2019-04-18 17:05:18 +0300

    style: add App styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
 import Dashboard from "./Dashboard";
+
+const S = {};
+S.App = styled.div`
+  padding: 28px 64px 72px;
+  background: white;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <S.App>
         <Dashboard />
-      </div>
+      </S.App>
     );
   }
 }

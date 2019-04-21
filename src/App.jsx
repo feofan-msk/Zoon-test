659d220814bb1115aa80bb0d37edc54f4c3a29ae
Denis Feofanov <omail4me@gmail.com>
2019-04-21 18:19:16 +0300

    style: add breakpoint and app style

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import { Box } from "@rebass/grid";
 
 import Dashboard from "./Dashboard";
 import Companies from "./Companies";
 
 const S = {};
-S.App = styled.div`
-  padding: 28px 64px 72px;
-  background: white;
+S.App = styled(Box)`
+  margin: auto;
+  padding-bottom: 72px;
+  max-width: 1200px;
 `;
 
-const App = ({ companies }) => (
-  <S.App>
-    <Dashboard />
+const theme = {
+  breakpoints: ["60em"]
+};
 
-    <Companies companies={companies} />
-  </S.App>
+const App = ({ companies }) => (
+  <ThemeProvider theme={theme}>
+    <S.App pt={[2, 4]} px={[2, 5]}>
+      <Dashboard />
+
+      <Companies companies={companies} />
+    </S.App>
+  </ThemeProvider>
 );
 
 export default App;
